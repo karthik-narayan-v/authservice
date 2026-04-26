d73f8984d2a856046c49cf38a1f03653af425024
Karthik <karthikgogul6@gmail.com>
2026-04-26 23:03:45 +0530

    Resend Email debugging

--- src/main/java/com/karthik/authservice/email/ResendEmailService.java
+++ src/main/java/com/karthik/authservice/email/ResendEmailService.java
@@ -49,6 +49,7 @@ public class ResendEmailService implements EmailService {
         try {
             restTemplate.postForEntity(url, request, String.class);
         } catch (Exception e) {
+            e.printStackTrace();
             throw new CustomException("Failed to send verification email", 500);
         }
     }
